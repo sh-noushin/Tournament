47cc0cc2707cbd98ccf1f42fb90e7d0e1e7be2d4
Nooshin Shokrollahi <nooshin.shokrollahi@bridging-it.de>
2023-06-03 13:05:17 +0200

    Participant and JumpAttempt classes are added

---
+++ Tournament.API/Tournament.API.Domain/Tournaments/JumpAttempt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.API.Domain.Core;
+
+namespace Tournament.API.Domain.Tournaments
+{
+    public class Jump : BaseEntity<int>
+    {
+        public int TournamentId { get; set; }
+        public int ParticipantId { get; set; }
+        public int Distance { get; set; }
+    }
+}
--- Tournament.API/Tournament.API.Domain/Tournaments/Tournament.cs
+++ Tournament.API/Tournament.API.Domain/Tournaments/Tournament.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tournament.API.Domain.Core;
+﻿using Tournament.API.Domain.Core;
 using Tournament.API.Domain.Shared.Tournaments;
 using Tournament.API.Domain.Tournaments.Ecxeptions;
 
@@ -12,7 +7,7 @@ namespace Tournament.API.Domain.Tournaments
     public class Tournament : BaseEntity<int>
     {
         public string? Name { get; private set; }
-
+        public List<JumpAttempt> Attempts { get; set; }
         public Tournament()
         {
 
@@ -24,7 +19,7 @@ namespace Tournament.API.Domain.Tournaments
 
         private void SetName(string name)
         {
-            if(name.Length > TournamentConsts.NameMaxLength)
+            if (name.Length > TournamentConsts.NameMaxLength)
             {
 
                 throw new TournamentNameLengthException();
