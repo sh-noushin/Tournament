2fdf5f80b1740d9442dbe0d217753dbbdff125be
Nooshin Shokrollahi <nooshin.shokrollahi@bridging-it.de>
2023-06-05 18:25:04 +0200

    Client project is created and NSwag setting is added

--- Tournament.API/Tournament.API.Web/Program.cs
+++ Tournament.API/Tournament.API.Web/Program.cs
@@ -18,6 +18,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+builder.Services.AddSwaggerDocument(settings =>
+{
+    settings.PostProcess = document =>
+    {
+        document.Info.Version = "v1";
+        document.Info.Title = "Tournament API";
+        document.Info.Description = "";
+    };
+});
 builder.Services.AddDbContext<TournamentAPIDbContext>(options =>
          options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -38,8 +48,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
